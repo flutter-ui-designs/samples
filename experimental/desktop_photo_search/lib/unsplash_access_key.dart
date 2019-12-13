5e2aac5e30f52dd3e391b90f2515ea234a26bc98
Mehul Aggarwal <53606356+mehulagg@users.noreply.github.com>
2019-12-13 20:20:10 +0530

    Update unsplash_access_key.dart

--- experimental/desktop_photo_search/lib/unsplash_access_key.dart
+++ experimental/desktop_photo_search/lib/unsplash_access_key.dart
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 // TODO: Retrieve an API Access Key from https://unsplash.com/developers
-const String unsplashAccessKey = '';
+const String unsplashAccessKey = 'a1bbe048c2f7ce04c467e7bad753ceb0aa3494563e39401f191a60f6f693d532';
 
 // The application name for the above API Access Key.
-const unsplashAppName = '';
+const unsplashAppName = 'Main';
